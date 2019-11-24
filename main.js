82aa1d6ac9853fa6bd9f9862cb2f4456e7e34c3e
Dark Angel <jonathanyombo@gmail.com>
2019-11-24 21:54:41 +0200

    [UPDATE] add flip2back func
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ let mem_tile_ids = [];
 let tiles_flipped = 0;
 
 Array.prototype.shuffle = function () {
-  let i = this.length, j, temp;
-  while (--i > 0){
-      j = Math.floor(Math.random() * (i+1));
-      temp = this[j];
-      this[j] = this[i];
-      this[i] = temp;
-  }
+    let i = this.length, j, temp;
+    while (--i > 0) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
 };
 
 function newBoard() {
@@ -21,4 +21,43 @@ function newBoard() {
         outpout += '<div id="tile_' + i + '" onclick="memFliptile(this, \'' + mem_array[i] + '\')"></div>'
     }
     document.getElementById('memboard').innerHTML = outpout;
+}
+
+function memFliptile(tile, val) {
+    if ((tile.innerHTML === "") && (mem_val.length < 2)) {
+        tile.style.background = '#ffffff';
+        tile.innerHTML = val;
+        if (mem_val.length === 0) {
+            mem_val.push(val);
+            mem_tile_ids.push(tile.id);
+        } else if (mem_val.length === 1) {
+            mem_val.push(val);
+            mem_tile_ids.push(tile.id);
+            if (mem_val[0] === mem_val[1]) {
+                tiles_flipped += 2;
+                mem_val = [];
+                mem_tile_ids = [];
+                if (tiles_flipped === mem_array.length) {
+                    alert('Tableau effacé... Encours de regeneration');
+                    document.getElementById('memboard').innerHTML = "";
+                    newBoard();
+                }
+            } else {
+                function flip2back() {
+                    let t1 = document.getElementById(mem_tile_ids[0]);
+                    let t2 = document.getElementById(mem_tile_ids[1]);
+
+                    t1.style.background = 'url(logo.svg) no-repeat';
+                    t2.style.background = 'url(logo.svg) no-repeat';
+                    t1.innerHTML = "";
+                    t2.innerHTML = "";
+
+                    mem_val = [];
+                    mem_tile_ids = [];
+                }
+
+                setTimeout(flip2back, 700);
+            }
+        }
+    }
 }
