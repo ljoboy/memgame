f592d3dbcfad628d1bc949677890c27e3d32aaeb
Dark Angel <jonathanyombo@gmail.com>
2019-11-24 20:49:40 +0200

    [UPDATE] shuffle prototype add to array & initialise variable
    Signed-off-by: Dark Angel <jonathanyombo@gmail.com>

---
+++ main.js
@@ -0,0 +1,14 @@
+let mem_array = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K'];
+let mem_val = [];
+let mem_tile_ids = [];
+let tiles_flipped = 0;
+
+Array.prototype.shuffle = function () {
+  let i = this.length, j, temp;
+  while (--i > 0){
+      j = Math.floor(Math.random() * (i+1));
+      temp = this[j];
+      this[j] = this[i];
+      this[i] = temp;
+  }
+};
